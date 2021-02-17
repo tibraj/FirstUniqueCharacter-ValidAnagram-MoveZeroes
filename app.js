de417503ba6cc5293be463680b910e4cd4ee3d35
tibraj <tibraj94@gmail.com>
2021-02-17 17:10:04 -0500

    valid anagram

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ function firstUniq(s) {
     for (let i = 0; i < s.length; ++i) if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) return i;
     return -1;
 }
+
+//Valid Anagram
+const isAnagram = (s, t) => s.split('').sort().join('') === t.split('').sort().join('');
+
+
+//Move Zeroes
+
